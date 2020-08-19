7bd4f6e0dde11ea1a1a3fac01e645a1d55f99839
mdiaz-8 <69877314+mdiaz-8@users.noreply.github.com>
2020-08-18 20:14:14 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+* Author: May Diaz
+*
+* A simple hello world program
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+  printf("Hello World!\n");
+
+  return 0;
+}
